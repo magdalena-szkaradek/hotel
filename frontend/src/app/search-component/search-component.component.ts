62bb0b3e5fdc2e4c4fb6142abdf7b7847624ae9d
Kaja <kajaslomska04@gmail.com>
2018-12-01 22:21:16 +0100

    adding getting dates between which rooms needs to be find

--- frontend/src/app/search-component/search-component.component.ts
+++ frontend/src/app/search-component/search-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RoomService } from '../services/room.service';
+
 
 @Component({
   selector: 'app-search-component',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponentComponent implements OnInit {
 
-  constructor() { }
+  availRooms: any;
+  constructor(
+    private roomService: RoomService
+  ) {   
+  }
 
   ngOnInit() {
   }
+ 
 
+  searchForFreeRooms({value,valid}){
+    
+    console.log(value);
+
+    // if(valid){
+
+    //   this.roomService.availableRooms(value).subscribe(res =>{
+
+
+    //     this.roomService.getRooms().subscribe(rooms => {
+    //       this.roomService.roomsBS.next(rooms);
+    //     })
+
+    //   })
+    // }else{
+    //   console.log('Searching for available rooms went wrong');
+    // }
+  }
 }
