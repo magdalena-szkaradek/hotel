41463743cbaa2601b2f3cddb390b80a842e9fd30
Asia Ziobrowska <joanna.ziobrowska@student.uj.edu.pl>
2018-12-08 20:43:28 +0100

    Deleting reservations in client profile

--- frontend/src/app/components/user-reservations/user-reservations.component.ts
+++ frontend/src/app/components/user-reservations/user-reservations.component.ts
@@ -8,29 +8,62 @@ import {Reservation} from './Reservation';
   selector: 'app-admin-reservations',
   templateUrl: './user-reservations.component.html',
   styleUrls: ['./user-reservations.component.css']
+  
 })
+
 export class UserReservationsComponent implements OnInit {
 
   reservations: Reservation[];
   successMsg: boolean = false;
   errorMsg: boolean = false;
+  date: any;
 
   constructor(
     private router: Router,
     private reservationService: ReservationService
-  ) {
-  }
+  ) { }
 
+ 
   ngOnInit() {
     const UserID = {
       'userId': localStorage.getItem('userID')
     };
+
     this.reservationService.getUserReservation(UserID.userId.toString()).subscribe(reservations => {
       this.reservations = reservations;
+      this.date = new Date().toISOString();
+     
+  
     });
+  
   }
 
-  deleteReservation(id: any) {
-    this.reservationService.deleteReservation(id).subscribe();
+  deleteReservation(id) {
+    if (confirm('Confirm deletion')) {
+
+      this.reservationService.deleteReservation(id).subscribe(res => {
+      
+      this.successMsg = true;
+      setTimeout(function() {
+          this.successMsg = false;
+      }.bind(this),2000);
+
+        const UserID = {
+          'userId': localStorage.getItem('userID')
+        };
+        
+        this.reservationService.getUserReservation(UserID.userId.toString()).subscribe(reservations => {
+          this.reservations = reservations;
+        })
+
+      },
+      error => {
+        this.errorMsg = true;
+        console.log("An error occured");       
+    });
+    }
   }
+    
 }
+
+
