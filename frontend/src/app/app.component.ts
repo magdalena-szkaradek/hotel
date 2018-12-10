d16df6b3b334ee7d3eeb094b06c9e1b796ac5aeb
Magdalena Szkaradek <magdalena.szkaradek@o2.pl>
2018-12-10 02:31:30 +0100

    add navbar for employee

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+  private isEmployee: boolean;
 
   get front() {
     if (localStorage.getItem("user") === "\"admin\"") {
@@ -15,4 +16,7 @@ export class AppComponent {
     return true;
   }
 
+  ngOnInit(){
+    this.isEmployee = localStorage.getItem('role') === 'employee';
+  }
 }
